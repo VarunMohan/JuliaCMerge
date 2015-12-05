02ab225193f44f7c16b94cf9948c701769bd9b31
Berj Chilingirian <berjc@mit.edu>
2015-12-05 15:17:09 -0500

    use random UInt32

--- src/merge/julia/sort_test.jl
+++ src/merge/julia/sort_test.jl
@@ -11,19 +11,17 @@ NUM_TESTS = 1
 function gen_test(n)
     A = zeros(UInt32, n)
     for i in 1:n
-        A[i] = rand(1:n)
+        A[i] = rand(UInt32)
     end
     A
 end
 
-#=
 println("Testing InsertionSort")
 for i in 1:NUM_TESTS
     A = gen_test(N)
     B = copy(A)
     @test MySort.InsertionSort!(A, 1, N) == sort!(B, alg=Sort.InsertionSort)
 end
-=#
 
 println("Testing MergeSort")
 for i in 1:NUM_TESTS
