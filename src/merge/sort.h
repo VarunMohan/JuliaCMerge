e0daab7bc7cbbc2f6cdf089c8e9b31458062fa3a
William Qian <wqian94@mit.edu>
2015-12-05 14:19:47 -0500

    sort.h with *j

--- src/merge/sort.h
+++ src/merge/sort.h
@@ -11,7 +11,7 @@ void parallel_merge_sort(uint32_t* arr, uint32_t* arrend);
 void serial_merge_sort(uint32_t* arr, uint32_t* arrend);
 
 static void insertion_sort(uint32_t* arr, uint32_t* arrend) {
-  for (uint32_t* i = arr + 1, j; i < arrend; i++) {
+  for (uint32_t *i = arr + 1, *j; i < arrend; i++) {
     uint32_t x = *i;
     for (j = i; j > arr; j--) {
       if (*j < *(j - 1)) {
