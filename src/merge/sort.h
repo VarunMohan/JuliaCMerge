194f7794e17b710f7ed9f660c84e635757fc62a2
William Qian <wqian94@mit.edu>
2015-12-05 14:17:35 -0500

    insertion sort???

---
+++ src/merge/sort.h
@@ -0,0 +1,28 @@
+#ifndef MERGE_H
+#define MERGE_H
+
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdbool.h>
+
+void parallel_merge_sort(uint32_t* arr, uint32_t* arrend);
+void serial_merge_sort(uint32_t* arr, uint32_t* arrend);
+
+static void insertion_sort(uint32_t* arr, uint32_t* arrend) {
+  for (uint32_t* i = arr + 1, j; i < arrend; i++) {
+    uint32_t x = *i;
+    for (j = i; j > arr; j--) {
+      if (*j < *(j - 1)) {
+        *j = *(j - 1);
+        continue;
+      }
+      break;
+    }
+    *j = x;
+  }
+}
+
+
+#endif
