a6ba4960c6d258550a554740622d369a6571ff88
Berj Chilingirian <berjc@mit.edu>
2015-12-05 15:10:57 -0500

    inline

--- src/merge/julia/sort.jl
+++ src/merge/julia/sort.jl
@@ -29,7 +29,8 @@ function MergeSort!(v::Array{UInt32,1}, lo::Int, hi::Int, t=similar(v,0))
         MergeSort!(v, lo,  m, t)
         MergeSort!(v, m+1, hi, t)
 
-        i, j = 1, lo
+	merge(v, lo, hi, t)
+#=        i, j = 1, lo
         while j <= m
             t[i] = v[j]
             i += 1
@@ -51,10 +52,38 @@ function MergeSort!(v::Array{UInt32,1}, lo::Int, hi::Int, t=similar(v,0))
             v[k] = t[i]
             k += 1
             i += 1
-        end
+        end=#
     end
 
     return v
+end
+
+@inline function merge(v::Array{UInt32,1}, lo::Int, hi::Int, t::Array{UInt32,1})
+    m = (lo+hi)>>>1
+
+    i, j = 1, lo
+    while j <= m
+	t[i] = v[j]
+	i += 1
+	j += 1
+    end
+
+    i, k = 1, lo
+    while k < j <= hi
+	if v[j] < t[i]
+	    v[k] = v[j]
+	    j += 1
+	else
+	    v[k] = t[i]
+	    i += 1
+	end
+	k += 1
+    end
+    while k < j
+	v[k] = t[i]
+	k += 1
+	i += 1
+    end
 end
 
 end
