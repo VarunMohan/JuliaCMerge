7dd5ca09c997f867ecc3f127733d0f4549e233a3
William Qian <wqian94@mit.edu>
2015-12-05 16:26:07 -0500

    updated testing suite

--- src/merge/c/merge_test.c
+++ src/merge/c/merge_test.c
@@ -12,7 +12,7 @@
 #define DEFAULT_NUM_CHECKS 3
 
 int uint32_t_compare (const void* a, const void* b) {
-  return (*(int*)a - *(int*)b);
+    return (*(int*)a - *(int*)b);
 }
 
 double get_time() {
@@ -22,68 +22,59 @@ double get_time() {
     return res;
 }
 
-double time(void (*func)(uint32_t *start, uint32_t *end), uint32_t *data, size_t size) {
+double benchmark(void (*func)(uint32_t *start, uint32_t *end), uint32_t *data, size_t size) {
     double start = get_time();
     func(data, data + size);
     return get_time() - start;
 }
 
 int main(int argc, char *argv[]) {
-  srand(time(NULL));
-  //__cilkrts_set_param("nworkers", "8");
+    srand(time(NULL));
+    //__cilkrts_set_param("nworkers", "8");
 
-  size_t arr_size = DEFAULT_ARR_SIZE;
-  size_t num_checks = DEFAULT_NUM_CHECKS;
+    size_t arr_size = DEFAULT_ARR_SIZE;
+    size_t num_checks = DEFAULT_NUM_CHECKS;
 
-  if (argc > 1) {
-    num_checks = atoi(argv[1]);
-    if (argc > 2) {
-      arr_size = atoi(argv[2]);
-    }
-  }
-
-  printf("Performing %lu Sorting Tests on Array of Size %lu\n", num_checks, arr_size);
-
-  uint32_t* serial_arr = (uint32_t*)malloc(sizeof(uint32_t) * arr_size);
-  uint32_t* parallel_arr = (uint32_t*)malloc(sizeof(uint32_t) * arr_size);
-  uint32_t* qsort_arr = (uint32_t*)malloc(sizeof(uint32_t) * arr_size);
-
-  double serial_time = 0;
-  double parallel_time = 0;
-  double qsort_time = 0;
-
-  for (size_t checks = 1; checks <= num_checks; checks++) {
-    printf("Check %lu\n", checks);
-    for (size_t i = 0; i < arr_size; i++) {
-      serial_arr[i] = parallel_arr[i] = qsort_arr[i] = rand() % 100;
-    }
-
-    double start_time = get_time();
-    serial_merge_sort(serial_arr, serial_arr + arr_size);
-    double serial_end = get_time();
-    parallel_merge_sort(parallel_arr, parallel_arr + arr_size);
-    double parallel_end = get_time();
-    qsort(qsort_arr, arr_size, sizeof(uint32_t), uint32_t_compare);
-    double qsort_end = get_time();
-
-    serial_time += serial_end - start_time;
-    parallel_time += parallel_end - serial_end;
-    qsort_time += qsort_end - parallel_end;
-
-    for (size_t i = 0; i < arr_size; i++) {
-      if (qsort_arr[i] != serial_arr[i] || qsort_arr[i] != parallel_arr[i]) {
-        if (qsort_arr[i] != serial_arr[i]) {
-          printf("Fail! Serial Merge is Incorrect\n");
-        } else {
-          printf("Fail! Parallel Merge is Incorrect\n");
+    if (argc > 1) {
+        num_checks = atoi(argv[1]);
+        if (argc > 2) {
+            arr_size = atoi(argv[2]);
         }
-        return 0;
-      }
     }
-  }
 
-  printf("Success\n");
-  printf("Average time for each Serial Merge Sort: %.6fs\n", serial_time/num_checks);
-  printf("Average time for each Parallel Merge Sort: %.6fs\n", parallel_time/num_checks);
-  printf("Average time for each Builtin QSort: %.6fs\n", qsort_time/num_checks);
+    printf("Performing %lu Sorting Tests on Array of Size %lu\n", num_checks, arr_size);
+
+    uint32_t* orig_arr = (uint32_t*)malloc(sizeof(uint32_t) * arr_size);
+    uint32_t* data_arr = (uint32_t*)malloc(sizeof(uint32_t) * arr_size);
+    uint32_t* qsort_arr = (uint32_t*)malloc(sizeof(uint32_t) * arr_size);
+
+    #define NVARIANTS 3
+    void (*funcs[4]) = {NULL, serial_merge_sort, naive_serial_merge_sort, parallel_merge_sort};
+    double times[NVARIANTS + 1] = {0, 0, 0, 0};
+    char *names[NVARIANTS + 1] = {"Builtin Quick", "Serial Merge", "Naive Serial Merge", "Parallel Merge"};
+
+    for (size_t checks = 1; checks <= num_checks; checks++) {
+        printf("Check %lu\n", checks);
+        for (size_t i = 0; i < arr_size; i++)
+            orig_arr[i] = qsort_arr[i] = rand() % 100;
+
+        double start_time = get_time();
+        qsort(qsort_arr, arr_size, sizeof(uint32_t), uint32_t_compare);
+        double end_time = get_time();
+        times[0] += end_time - start_time;
+
+        for (int i = 1; i <= NVARIANTS; i++) {
+            memcpy(data_arr, orig_arr, arr_size * sizeof(uint32_t));
+            times[i] += benchmark(funcs[i], data_arr, arr_size);
+            for (size_t j = 0; j < arr_size; j++)
+                if (qsort_arr[i] != data_arr[i]) {
+                    printf("Fail! %s is incorrect\n", names[i]);
+                    return 0;
+                }
+        }
+    }
+
+    printf("Success\n");
+    for (int i = 0; i <= NVARIANTS; i++)
+        printf("Average time for each %s Sort: %.6fs\n", names[i], times[i] / num_checks);
 }
