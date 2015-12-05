a5ec09e7cfc87e764ae3b08674eadd81243694a4
Varun Mohan <vmohan@mit.edu>
2015-12-05 15:37:51 -0500

    standardized parallel code

--- src/merge/c/parallel_merge.c
+++ src/merge/c/parallel_merge.c
@@ -19,38 +19,29 @@ static void serial_merge(uint32_t* C, uint32_t* A, uint32_t* Aend, uint32_t* B, 
   }
 }
 
-static void parallel_merge(uint32_t* C, uint32_t* A, uint32_t* Aend, uint32_t* B, uint32_t* Bend) {
-  serial_merge(C, A, Aend, B, Bend);
-}
-
-static void merge_sort_helper(uint32_t* arr, uint32_t n, int32_t* aux, bool sort_in_arr) {
+static void merge_sort_helper(uint32_t* arr, uint32_t n, int32_t* aux) {
   if (n <= SMALL_THRESHOLD) {
-    if (sort_in_arr) insertion_sort(arr, arr + n);
-    else insertion_sort(aux, aux + n);
+    insertion_sort(arr, arr + n);
     return;
   }
 
   if (n < (1 << 11)) {
-    merge_sort_helper(arr, n/2, aux, !sort_in_arr);
-    merge_sort_helper(arr + n/2, n - n/2, aux + n/2, !sort_in_arr);
+    merge_sort_helper(aux, n/2, arr);
+    merge_sort_helper(aux + n/2, n - n/2, arr + n/2);
   } else {
-    cilk_spawn merge_sort_helper(arr, n/2, aux, !sort_in_arr);
-    merge_sort_helper(arr + n/2, n - n/2, aux + n/2, !sort_in_arr);
+    cilk_spawn merge_sort_helper(aux, n/2, arr);
+    merge_sort_helper(aux + n/2, n - n/2, arr + n/2);
     cilk_sync;
   }
 
-  if (sort_in_arr) {
-    parallel_merge(arr, aux, aux + n/2, aux + n/2, aux + n);
-  } else {
-    parallel_merge(aux, arr, arr + n/2, arr + n/2, arr + n);
-  }
+  serial_merge(arr, aux, aux + n/2, aux + n/2, aux + n);
 }
 
 void parallel_merge_sort(uint32_t* arr, uint32_t* arrend) {
   uint32_t n = arrend - arr;
   uint32_t *aux = (uint32_t*)malloc(sizeof(uint32_t) * (n)); //auxilliary memory for merge sort
 
-  merge_sort_helper(arr, n, aux, true);
+  merge_sort_helper(arr, n, aux);
 
   free(aux);
 }
