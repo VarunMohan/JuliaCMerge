bd3f78abb4b2b7ce4d700df62ae9bea827415ca5
William Qian <wqian94@mit.edu>
2015-12-08 16:00:14 -0500

    Benchmarks 3

--- src/scripts/run_bench.py
+++ src/scripts/run_bench.py
@@ -2,6 +2,7 @@
 
 import matplotlib.pyplot as plt
 from subprocess import call
+import sys
 
 AWSRUN = ""#"awsrun"
 C_FN_O = "c_output"
@@ -11,124 +12,124 @@ NUM_TRIALS = 10
 N = 2**23
 
 def run_c_impl(num_cilkwrks):
-  global AWSRUN
-  global C_FN_O
+    global AWSRUN
+    global C_FN_O
 
-  call("cd ../merge/c/; make clean; make -B", shell=True)
-  print call(AWSRUN + "export CILK_NWORKERS=%(num_cilkwrks)d && ../merge/c/test | tee " % locals() \
-       + C_FN_O, shell=True)
+    call("cd ../merge/c/; make clean; make -B", shell=True)
+    print call(AWSRUN + "export CILK_NWORKERS=%(num_cilkwrks)d && ../merge/c/test | tee " % locals() \
+         + C_FN_O, shell=True)
 
 def read_c_output(d, num_cilkwrks):
-  global C_FN_O
+    global C_FN_O
 
-  with open(C_FN_O, "r") as f:
-    for line in f:
-      print line,
-      if line.startswith("Average"):
-        if "Naive Serial Merge Sort" in line:
-          d['c']['serial'] = float(line.split(" ")[-1].rstrip("s\n"))
-        elif "Optimized Serial Merge Sort" in line:
-          d['c']['serial_opt'] = float(line.split(" ")[-1].rstrip("s\n"))
-        elif "Optimized Parallel Merge Sort" in line:
-          d['c']['parallel'][num_cilkwrks] = float(line.split(" ")[-1].rstrip("s\n"))
+    with open(C_FN_O, "r") as f:
+        for line in f:
+            print line,
+            if line.startswith("Average"):
+                if "Naive Serial Merge Sort" in line:
+                    d['c']['serial'] = float(line.split(" ")[-1].rstrip("s\n"))
+                elif "Optimized Serial Merge Sort" in line:
+                    d['c']['serial_opt'] = float(line.split(" ")[-1].rstrip("s\n"))
+                elif "Optimized Parallel Merge Sort" in line:
+                    d['c']['parallel'][num_cilkwrks] = float(line.split(" ")[-1].rstrip("s\n"))
 
 def collect_c_data(d):
-  for i in range(8):
-    #run_c_impl(i + 1)
-    read_c_output(d, i + 1)
+    for i in range(8):
+        run_c_impl(i + 1)
+        read_c_output(d, i + 1)
 
 def run_j_impl():
-  global J_FN_O
+    global J_FN_O
 
-  call("julia MyBenchmarks.jl | tee " + J_FN_O, shell=True)
+    call("julia MyBenchmarks.jl | tee " + J_FN_O, shell=True)
 
 def read_j_output(d):
-  global J_FN_O
+    global J_FN_O
 
-  with open(J_FN_O, "r") as f:
-    for line in f:
-        if "MySerialSort" in line:
-          d['j']['serial'] = float(line.split(" ")[-1].rstrip())
-        elif "MySerialOptSort" in line:
-          d['j']['serial_opt'] = float(line.split(" ")[-1].rstrip())
-        elif "MyParallelSort" in line:
-          d['j']['parallel_cpy'] = float(line.split(" ")[-1].rstrip())
-        elif "MyParallelSASort" in line:
-          num_procs = int(line.split(" ")[-2])
-          d['j']['parallel'][num_procs] = float(line.split(" ")[-1].rstrip())
+    with open(J_FN_O, "r") as f:
+        for line in f:
+            if "MySerialSort" in line:
+                d['j']['serial'] = float(line.split(" ")[-1].rstrip())
+            elif "MySerialOptSort" in line:
+                d['j']['serial_opt'] = float(line.split(" ")[-1].rstrip())
+            elif "MyParallelSort" in line:
+                d['j']['parallel_cpy'] = float(line.split(" ")[-1].rstrip())
+            elif "MyParallelSASort" in line:
+                num_procs = int(line.split(" ")[-2])
+                d['j']['parallel'][num_procs] = float(line.split(" ")[-1].rstrip())
 
 def collect_j_data(d):
-  #run_j_impl()
-  read_j_output(d)
+    run_j_impl()
+    read_j_output(d)
 
 def stats_summary(d):
-  print d
-  for l in ['j']:
-    lang = "Julia" if l == "j" else "C"
-    print lang + " Implementation Results"
-    for k in d[l].keys():
-      if k != 'parallel':
-        result = d[l][k]
-        print "\t%(k)s : %(result)f" % locals()
-      else:
-        print "\tParallel Performance by Number of Workers"
-        for i in range(1, 9):
-          result = d[l][k][i]
-          print "\t\t%(i)d : %(result)f" % locals()
+    print d
+    for l in ['j']:
+        lang = "Julia" if l == "j" else "C"
+        print lang + " Implementation Results"
+        for k in d[l].keys():
+            if k != 'parallel':
+                result = d[l][k]
+                print "\t%(k)s : %(result)f" % locals()
+            else:
+                print "\tParallel Performance by Number of Workers"
+                for i in range(1, 9):
+                    result = d[l][k][i]
+                    print "\t\t%(i)d : %(result)f" % locals()
 
 def plot_data(d):
-  plt.subplot(1, 2, 1)
-  x = range(1,9)
-  y1 = [d['c']['parallel'][i] for i in x]
-  y2 = [d['j']['parallel'][i] for i in x]
+    plt.subplot(1, 2, 1)
+    x = range(1,9)
+    y1 = [d['c']['parallel'][i] for i in x]
+    y2 = [d['j']['parallel'][i] for i in x]
 
-  plt.plot(x, y1, 'bs-', label='C')
-  plt.plot(x, y2, 'g^-', label='Julia')
-  plt.title("Parallel Runtime vs. Number of Cores")
-  plt.xlabel("Number of Cores")
-  plt.ylabel("Runtime (secs)")
-  plt.legend(loc='right')
-  #plt.show()
+    plt.plot(x, y1, 'bs-', label='C')
+    plt.plot(x, y2, 'g^-', label='Julia')
+    plt.title("Parallel Runtime vs. Number of Cores")
+    plt.xlabel("Number of Cores")
+    plt.ylabel("Runtime (secs)")
+    plt.legend(loc='right')
+    #plt.show()
 
-  plt.subplot(1, 2, 2)
-  x = range(1,9)
-  y1 = [d['c']['serial_opt'] / d['c']['parallel'][i] for i in x]
-  y2 = [d['j']['serial_opt'] / d['j']['parallel'][i] for i in x]
-  plt.plot(x, y1, 'bs-', label='C')
-  plt.plot(x, y2, 'g^-', label='Julia')
-  plt.title("Parallel Speedup vs. Number of Cores")
-  plt.xlabel("Number of Cores")
-  plt.ylabel("Optimized Serial / Parallel Runtime")
-  plt.legend(loc='right')
-  plt.show()
+    plt.subplot(1, 2, 2)
+    x = range(1,9)
+    y1 = [d['c']['serial_opt'] / d['c']['parallel'][i] for i in x]
+    y2 = [d['j']['serial_opt'] / d['j']['parallel'][i] for i in x]
+    plt.plot(x, y1, 'bs-', label='C')
+    plt.plot(x, y2, 'g^-', label='Julia')
+    plt.title("Parallel Speedup vs. Number of Cores")
+    plt.xlabel("Number of Cores")
+    plt.ylabel("Optimized Serial / Parallel Runtime")
+    plt.legend(loc='right')
+    plt.show()
 
 def cleanup():
-  global C_FN_O
-  global J_FN_O
+    global C_FN_O
+    global J_FN_O
 
-  call("rm " + C_FN_O + " " + J_FN_O, shell=True)
+    call("rm " + C_FN_O + " " + J_FN_O, shell=True)
 
 data = {
-  'c' : {
-    'serial'     : None,
-    'serial_opt' : None,
-    'parallel'   : {
-      i : None for i in range(1,9)
+    'c' : {
+        'serial'         : None,
+        'serial_opt'     : None,
+        'parallel'       : {
+            i : None for i in range(1,9)
+        }
+    },
+    'j' : {
+        'serial'         : None,
+        'serial_opt'     : None,
+        'parallel_cpy'   : None,
+        'parallel'       : {
+            i : None for i in range(1,9)
+        }
     }
-  },
-  'j' : {
-    'serial'       : None,
-    'serial_opt'   : None,
-    'parallel_cpy' : None,
-    'parallel'     : {
-      i : None for i in range(1,9)
-    }
-  }
 }
 
 if __name__ == "__main__":
-  #cleanup()
-  collect_c_data(data)
-  collect_j_data(data)
-  stats_summary(data)
-  plot_data(data)
+    cleanup()
+    collect_c_data(data)
+    collect_j_data(data)
+    stats_summary(data)
+    plot_data(data)
